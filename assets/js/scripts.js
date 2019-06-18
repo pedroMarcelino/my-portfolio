774887985517c400ac191f66ca8cd8cdf5be3f7f
pedroMarcelino <38365875+pedroMarcelino@users.noreply.github.com>
2019-06-18 17:55:08 -0300

    .

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,11 +1,11 @@
 $(function () {
     $(".navbar").attr("class", "navbar fixed-top navbar-expand-lg back-menu");
-    
+
     $(window).scroll(function () {
         var scroll = $(window).scrollTop();
 
         if (scroll > 100) {
-            $(".navbar").attr("class", "navbar fixed-top navbar-expand-lg navbar-dark bg-dark" );
+            $(".navbar").attr("class", "navbar fixed-top navbar-expand-lg navbar-dark bg-dark");
         } else {
             $(".navbar").attr("class", "navbar fixed-top navbar-expand-lg back-menu");
         }
@@ -27,38 +27,65 @@ $(function () {
         var assunto = $("#assunto").val();
         var menssagem = $("#menssagem").val();
 
-        var json = {
-            "nome": nome,
-            "email": email,
-            "assunto": assunto,
-            "menssagem": menssagem
+        if (nome == "" || email == "" || assunto == "" || menssagem == "") {
+            if (nome == "") {
+                $(".hidden1").css("display", "block");
+                $("#nome").keydown(function () {
+                    $(".hidden1").css("display", "none");
+                });
+            }else if (email == "") {
+                $(".hidden2").css("display", "block");
+                $("#email").keydown(function () {
+                    $(".hidden2").css("display", "none");
+                });
+            } else if (assunto == "") {
+                $(".hidden3").css("display", "block");
+                $("#assunto").keydown(function () {
+                    $(".hidden3").css("display", "none");
+                });
+            } else if (menssagem == "") {
+                $(".hidden4").css("display", "block");
+                $("#menssagem").keydown(function () {
+                    $(".hidden4").css("display", "none");
+                });
+            }
+            
+        } else {
+
+            var json = {
+                "nome": nome,
+                "email": email,
+                "assunto": assunto,
+                "menssagem": menssagem
+            }
+
+            $("#nome").val("")
+            $("#email").val("");
+            $("#assunto").val("");
+            $("#menssagem").val("");
+
+            $.ajax({
+                type: 'POST',
+                dataType: 'json',
+                url: 'sendmail.php',
+                async: true,
+                data: json,
+                success: function (data) {
+
+
+                    if (data.success == true) {
+                        swal("Prontinho!", "Recebi seu email, logo irei responder!!", "success");
+                    } else if (data.success == false) {
+                        swal("Oops...", "Ocorreu um problema, tente novamente!", "error");
+                    }
+
+
+                }
+            });
+
         }
 
-        $("#nome").val("")
-        $("#email").val("");
-        $("#assunto").val("");
-        $("#menssagem").val("");
 
-        $.ajax({
-            type: 'POST',
-            dataType: 'json',
-            url: 'sendmail.php',
-            async: true,
-            data: json,
-            success: function (data) {
-
-
-                if (data.success == true) {
-                    swal("Prontinho!", "Recebi seu email, logo irei responder!!", "success");
-                } else if (data.success == false) {
-                    swal("Oops...", "Ocorreu um problema, tente novamente!", "error");
-                }
-
-
-            }
-        });
-
-        console.log(json);
     })
 
 
